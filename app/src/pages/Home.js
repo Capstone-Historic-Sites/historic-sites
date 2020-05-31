d3a7acebbbee29e432b7996a5b9d4900360f10d8
Jeffrey Klein <JeffreyjkKlein@gmail.com>
2020-05-31 16:07:27 -0600

    added to home.js and upload

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -6,6 +6,17 @@ export const Home = () => {
     <>
       <NavBar />
       <h1>Home</h1>
+      <div>
+        <h2 className="text-center"><strong>Historic Sites</strong></h2>
+      </div>
+      <div>
+        <h3 className="text-center"><strong>About</strong></h3>
+        <p className="text-center">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+          laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+          voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+          non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+      </div>
     </>
   )
 }
--- app/src/pages/Upload.js
+++ app/src/pages/Upload.js
@@ -8,6 +8,13 @@ export const Upload = () => {
       <NavBar />
       <ProfileSidebar />
       <h1>Upload Page</h1>
+      <h2 className="text-center"><strong>Upload Historic Sites</strong></h2>
+      <form>
+        <div className="form-group">
+          <label htmlFor="exampleFormControlInput1">Email address</label>
+          <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        </div>
+      </form>
     </>
   )
 }
