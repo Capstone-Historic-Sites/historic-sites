cbe4772f91fe030bd9a5386d416b9f5045f26ad2
Jeffrey Klein <JeffreyjkKlein@gmail.com>
2020-05-31 17:39:05 -0600

    added search bar to home and started adding form to upload page

--- app/src/pages/Upload.js
+++ app/src/pages/Upload.js
@@ -8,15 +8,19 @@ export const Upload = () => {
       <NavBar />
       <ProfileSidebar />
       <h1>Upload Page</h1>
-      <h2 className="text-center"><strong>Upload Historic Sites</strong></h2>
+      <h2 className="text-left"><strong>Upload Historic Sites</strong></h2>
       <form>
         <div className="form-group">
-          <label htmlFor="formGroupExampleInput">Example label</label>
+          <label htmlFor="formGroupExampleInput">Name of Historic Site</label>
           <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Example input"/>
         </div>
         <div className="form-group">
-          <label htmlFor="formGroupExampleInput2">Another label</label>
+          <label htmlFor="formGroupExampleInput2">Street Address</label>
           <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="Another input"/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="formGroupExampleInput2">Description</label>
+          <input type="textarea" className="form-control" id="formGroupExampleInput3" placeholder="Another input"/>
         </div>
       </form>
     </>
