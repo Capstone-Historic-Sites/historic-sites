d8917fe660b8993222f86e77df9ea9483c3f12a3
Ricardo Garcia <rgarcia654@cnm.edu>
2020-05-31 14:55:47 -0600

    added faUserCircle to index page.

--- app/src/index.js
+++ app/src/index.js
@@ -10,9 +10,9 @@ import { Upload } from './pages/Upload'
 import { TravelList } from './pages/TravelList'
 import { FourOhFour } from './pages/FourOhFour'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import {  } from '@fortawesome/free-solid-svg-icons'
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
 
-library.add()
+library.add( faUserCircle )
 
 const Routing = () => (
   <>
