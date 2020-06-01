18c3138fa97dd5c981111d7c54ac3bdb19c3af45
Jeffrey Klein <JeffreyjkKlein@gmail.com>
2020-05-31 18:24:21 -0600

    Added some styling to the home and upload pages and added some content and buttons to upload

--- app/src/pages/Upload.js
+++ app/src/pages/Upload.js
@@ -1,28 +1,42 @@
 import React from 'react'
 import { NavBar } from '../shared/components/NavBar'
 import { ProfileSidebar } from '../shared/components/ProfileSidebar'
+import Button from 'react-bootstrap/Button'
 
 export const Upload = () => {
   return (
     <>
       <NavBar />
       <ProfileSidebar />
-      <h1>Upload Page</h1>
-      <h2 className="text-left"><strong>Upload Historic Sites</strong></h2>
-      <form>
-        <div className="form-group">
-          <label htmlFor="formGroupExampleInput">Name of Historic Site</label>
-          <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Example input"/>
-        </div>
-        <div className="form-group">
-          <label htmlFor="formGroupExampleInput2">Street Address</label>
-          <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="Another input"/>
-        </div>
-        <div className="form-group">
-          <label htmlFor="formGroupExampleInput2">Description</label>
-          <input type="textarea" className="form-control" id="formGroupExampleInput3" placeholder="Another input"/>
-        </div>
-      </form>
+      <div>
+        <h2 className="text-left"><strong>Upload Historic Sites</strong></h2>
+        <form>
+          <div className="form-group">
+            <label htmlFor="formGroupExampleInput">Name of Historic Site</label>
+            <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Example input"/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="formGroupExampleInput2">Street Address</label>
+            <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="Another input"/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="formGroupExampleInput2">Description</label>
+            <input type="textarea" className="form-control" id="formGroupExampleInput3" placeholder="Another input"/>
+          </div>
+          <div className="d-flex justify-content-between">
+            <div className="mb-3" align="left">
+              <Button variant="primary" size="md">
+                Select Photos
+              </Button>{' '}
+            </div>
+            <div className="mb-3" align="right">
+              <Button variant="primary" size="md">
+                Upload
+              </Button>{' '}
+            </div>
+          </div>
+        </form>
+      </div>
     </>
   )
 }
