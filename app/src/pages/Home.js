8e48f4ca98001bc8d8f5dbecc96b97aae6c86662
Keith Funkhouser <kfunkhouser@cnm.edu>
2020-05-31 22:44:50 -0600

    added google fonts, updated home.js

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -6,31 +6,31 @@ export const Home = () => {
   return (
     <>
       <NavBar />
-      <div className="container">
-        <div className="title-section">
-          <div className="w-100">
-            <h1 className="text-center display-2" ><strong>Historic Sites</strong></h1>
+      <section className="title-section">
+        <div className="container">
+          <div className="text-center w-100">
+            <h1 className="display-2" >Historic Sites</h1>
             <div className="active-pink-3 active-pink-4 mb-4">
               <input className="form-control" type="text" placeholder="Search" aria-label="Search" />
             </div>
           </div>
         </div>
-        <div className="about-section">
-          <div>
-            <h3 className="text-center"><strong>About</strong></h3>
-            <p className="text-center">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+      </section>
+      <section className="about-section">
+        <div className="container text-center">
+          <h3><strong>About</strong></h3>
+          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
               ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
               laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
               voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
               non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-            <div className="mb-2" align="center">
-              <Button variant="primary" size="lg">
-                Create Profile
-              </Button>{' '}
-            </div>
+          <div className="mb-2">
+            <Button variant="primary" size="lg">
+              Create Profile
+            </Button>
           </div>
         </div>
-      </div>
+      </section>
     </>
   )
 }
