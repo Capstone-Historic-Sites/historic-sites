e0db6ac67f517217bdc1447d49cc9c9463ddf377
Ricardo Garcia <rgarcia654@cnm.edu>
2020-06-01 06:55:12 -0600

    added simple carousel to SearchResults.js

--- app/src/pages/SearchResults.js
+++ app/src/pages/SearchResults.js
@@ -1,26 +1,29 @@
 import React from 'react'
 import { NavBar } from '../shared/components/NavBar'
+import Carousel from 'react-bootstrap/Carousel';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+
 export const SearchResults = () => {
+
   return (
     <>
       <NavBar />
       <div className="container" style={{paddingTop: '1.5rem' }}>
-
+        {/*Navbar*/}
         <div className="row">
           <div className="col" >
             <input className="form-control" type="text" placeholder="Search" aria-label="Search" />
           </div>
         </div>
-
+        {/*Map View & List View*/}
         <div className="row pt-2">
           <div className="col pl-4" style={{fontSize: '1.1rem'}}>
             <FontAwesomeIcon icon="list" />
             <p className="d-inline-block pl-2">List View</p>
           </div>
           <div className="col d-flex justify-content-end mr-3" style={{fontSize: '1.1rem'}}>
-            <FontAwesomeIcon icon="map-marker-alt" className="mt-1"/>
+              <FontAwesomeIcon icon="map-marker-alt" className="mt-1"/>
             <p className="d-inline-block ml-1">Map View</p>
           </div>
         </div>
@@ -28,10 +31,48 @@ export const SearchResults = () => {
 
       <div className="container"><hr/></div>
 
+        {/*Sites Container*/}
       <div className="container">
+          {/*First Site*/}
         <div className="row mb-3">
           <div className="col-lg-6">
+            <Carousel>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://images.pexels.com/photos/2835436/pexels-photo-2835436.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <h3>First slide label</h3>
+                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://images.pexels.com/photos/319882/pexels-photo-319882.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                  alt="Third slide"
+                />
 
+                <Carousel.Caption>
+                  <h3>Second slide label</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://images.pexels.com/photos/531321/pexels-photo-531321.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                  alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                  <h3>Third slide label</h3>
+                  <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
           </div>
           <div className="col-lg-6">
             <h2>Site Name</h2>
@@ -40,9 +81,47 @@ export const SearchResults = () => {
           </div>
         </div>
 
+        <hr/>
+          {/*Second Site*/}
         <div className="row mb-3">
           <div className="col-lg-6">
+            <Carousel>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://images.pexels.com/photos/2835436/pexels-photo-2835436.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <h3>First slide label</h3>
+                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://images.pexels.com/photos/319882/pexels-photo-319882.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                  alt="Third slide"
+                />
 
+                <Carousel.Caption>
+                  <h3>Second slide label</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://images.pexels.com/photos/531321/pexels-photo-531321.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                  alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                  <h3>Third slide label</h3>
+                  <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
           </div>
           <div className="col-lg-6">
             <h2>Site Name</h2>
@@ -51,9 +130,47 @@ export const SearchResults = () => {
           </div>
         </div>
 
+        <hr/>
+          {/*Third Site*/}
         <div className="row mb-3">
           <div className="col-lg-6">
+            <Carousel>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://images.pexels.com/photos/2835436/pexels-photo-2835436.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <h3>First slide label</h3>
+                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://images.pexels.com/photos/319882/pexels-photo-319882.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                  alt="Third slide"
+                />
 
+                <Carousel.Caption>
+                  <h3>Second slide label</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://images.pexels.com/photos/531321/pexels-photo-531321.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                  alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                  <h3>Third slide label</h3>
+                  <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
           </div>
           <div className="col-lg-6">
             <h2>Site Name</h2>
