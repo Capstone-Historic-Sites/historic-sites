3d68c1822064cc58c383ff1eab79d94a430bfe7e
Keith Funkhouser <kfunkhouser@cnm.edu>
2020-06-19 09:47:15 -0600

    removed setupProxy from git

--- app/src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-const proxy = require('http-proxy-middleware')
-module.exports = function (app) {
-  app.use(proxy('/apis', {
-      logLevel: 'debug',
-      target: 'http://138.197.193.239:8080/',
-      changeOrigin: true,
-    }
-  ))
-}
