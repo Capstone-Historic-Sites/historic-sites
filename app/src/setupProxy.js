ade9aea186212b2c2042a3677f58ad4bc225ce54
Cole Alexander <cbielefeld@cnm.edu>
2020-06-19 09:49:42 -0600

    individual docker IP adress added to the setup proxy

--- app/src/setupProxy.js
+++ app/src/setupProxy.js
@@ -2,7 +2,7 @@ const proxy = require('http-proxy-middleware')
 module.exports = function (app) {
   app.use(proxy('/apis', {
       logLevel: 'debug',
-      target: 'http://138.197.193.239:8080/',
+      target: 'http://138.197.213.198:8080/',
       changeOrigin: true,
     }
   ))
