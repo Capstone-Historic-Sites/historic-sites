568381dd33c988c5e995206a0cd64329ec3ead90
Ricardo-boot <63118944+Ricardo-boot@users.noreply.github.com>
2020-06-21 17:58:46 -0600

    added profile-sidebar.js

---
+++ app/src/store/profile-sidebar.js
@@ -0,0 +1,8 @@
+import {createSlice} from '@reduxjs/toolkit'
+import {httpConfig} from '../utils/http-config'
+
+const slice = createSlice({
+  name: "profile-sidebar",
+  initialState: [],
+  reducers: {}
+})
